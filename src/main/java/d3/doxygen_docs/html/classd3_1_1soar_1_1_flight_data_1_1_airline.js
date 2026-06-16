7f730d4edb9d1d89aeb02e7c57f6179da5c4a1ed
DonixDNator <natucciodignazio@gmail.com>
2026-06-16 22:58:52 +0200

    Delete src/main/java/d3/doxygen_docs/html directory

--- src/main/java/d3/doxygen_docs/html/classd3_1_1soar_1_1_flight_data_1_1_airline.js
+++
@@ -1,8 +0,0 @@
-var classd3_1_1soar_1_1_flight_data_1_1_airline =
-[
-    [ "Airline", "classd3_1_1soar_1_1_flight_data_1_1_airline.html#a6639a9512972d8b7a042efd069bf9c8b", null ],
-    [ "Airline", "classd3_1_1soar_1_1_flight_data_1_1_airline.html#a243ec0ec6dbbb324c7333c4f2e84d4ab", null ],
-    [ "getAirlineName", "classd3_1_1soar_1_1_flight_data_1_1_airline.html#a5181162dbf9893e0c3b87f570e146c01", null ],
-    [ "setAirlineName", "classd3_1_1soar_1_1_flight_data_1_1_airline.html#a2f13af6e10b2091467f16e7674b226e4", null ],
-    [ "toString", "classd3_1_1soar_1_1_flight_data_1_1_airline.html#a63c9137e9ae9e694c6c89f5a53d9878d", null ]
-];
--- src/main/java/d3/doxygen_docs/html/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['help_0',['help',['../enumd3_1_1soar_1_1main_1_1_command.html#a6ffdcb57989a9f4b58e0783470efe4f5',1,'d3::soar::main::Command']]]
-];
