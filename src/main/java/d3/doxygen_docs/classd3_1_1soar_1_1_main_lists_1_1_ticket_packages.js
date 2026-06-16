bdf176392ae6fbe5a032e45f75a3962e55bdf19b
DonixDNator <natucciodignazio@gmail.com>
2026-06-16 23:44:34 +0200

    Add files via upload

---
+++ src/main/java/d3/doxygen_docs/classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.js
@@ -0,0 +1,10 @@
+var classd3_1_1soar_1_1_main_lists_1_1_ticket_packages =
+[
+    [ "TicketPackages", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#a95ba760fcaae444099401ec91773d7a8", null ],
+    [ "TicketPackages", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#aed7f13790ddc0325df4a3406d8859c2e", null ],
+    [ "addpackage", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#adc36f65f4e3c8a6efce2b832a457d1ce", null ],
+    [ "readTicketPackagesFromFile", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#a56153c68e8477ac9967208c3e43330eb", null ],
+    [ "removeTicketPackage", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#a77120101477dd7aee15ee3efd4846684", null ],
+    [ "setTicketPackages", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#a29f282541c429a7ba9a0a59f4787aa85", null ],
+    [ "toString", "classd3_1_1soar_1_1_main_lists_1_1_ticket_packages.html#a4ed034c1986877fa21829a3c572dd476", null ]
+];
---
+++ src/main/java/d3/doxygen_docs/classd3_1_1soar_1_1_user_data_1_1_session.js
@@ -0,0 +1,14 @@
+var classd3_1_1soar_1_1_user_data_1_1_session =
+[
+    [ "Session", "classd3_1_1soar_1_1_user_data_1_1_session.html#ad6f0f539640ab3542953ed28217d0aa0", null ],
+    [ "Session", "classd3_1_1soar_1_1_user_data_1_1_session.html#adf1ea32077d8cd55577d60cd00305eec", null ],
+    [ "getExpiry", "classd3_1_1soar_1_1_user_data_1_1_session.html#a0f05ed9b97b178f8d68a1a8a5dad82db", null ],
+    [ "getToken", "classd3_1_1soar_1_1_user_data_1_1_session.html#a5ef02133855bb5c0bd6c97948a37690e", null ],
+    [ "getUser", "classd3_1_1soar_1_1_user_data_1_1_session.html#a228e302e11e010efe35859584e6850ad", null ],
+    [ "isValid", "classd3_1_1soar_1_1_user_data_1_1_session.html#a836e4434912ba9455483fa7ac801d99e", null ],
+    [ "setExpiry", "classd3_1_1soar_1_1_user_data_1_1_session.html#ae6cf80cc6a5717e7560e817d62e265ae", null ],
+    [ "setToken", "classd3_1_1soar_1_1_user_data_1_1_session.html#a85c04c95344e13a83699889f4ce4d681", null ],
+    [ "setUser", "classd3_1_1soar_1_1_user_data_1_1_session.html#a4e70c9e4e8809279738669b947fc4d4d", null ],
+    [ "terminate", "classd3_1_1soar_1_1_user_data_1_1_session.html#ac615e612808d35e8fa56a8574eb104e9", null ],
+    [ "toString", "classd3_1_1soar_1_1_user_data_1_1_session.html#a2449d23eaec9fd195358e74f29820c10", null ]
+];
